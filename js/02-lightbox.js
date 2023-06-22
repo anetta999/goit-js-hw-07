98df91348746ed6ece71f13808aad4e4036fa904
anetta999 <124580023+anetta999@users.noreply.github.com>
2023-06-22 18:08:29 +0300

    Completed task 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+console.log(galleryContainer);
+
+function createGalleryItemsMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src=${preview} alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  createGalleryItemsMarkup(galleryItems)
+);
+console.dir(galleryContainer);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+gallery.on("show.simplelightbox", function () {
+  // Do something…
+});
+
+gallery.on("error.simplelightbox", function (e) {
+  console.log(e); // Some usefull information
+});
